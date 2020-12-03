54fbb354d355138cd07c658532b15f1b52ffdcf3
Vishwaram H <kingvichu362@gmail.com>
2020-12-03 23:50:43 +0530

    Day 7

--- day7/beaddobject.js
+++ day7/beaddobject.js
@@ -1,12 +1,14 @@
 // Javascript prototype method to add object to the array of objects
-Array.prototype.beAddObject = function(object) {
-  this.push(object);
+Array.prototype.beAddObject = function(key, value) {
+  this.map((object) =>{
+    object[key] = value;
+  });
   return this;
 }
 
 var input = [
-  {name: "John"},
-  {age: 26},
+  {name: "John", age: 26},
+  {name: "Siva", age: 30},
 ];
 
-console.log(input.beAddObject({Country: "India"}));
+console.log(input.beAddObject("Country", "India"));
