9a89df2da5fb1d1e32fc3c3f8096f7456786e1e4
Vishwaram H <kingvichu362@gmail.com>
2020-12-04 00:34:47 +0530

    Day 6 changes

--- day6/befindobject.js
+++ day6/befindobject.js
@@ -1,16 +1,32 @@
 // Javascript prototype to find a particular object from an array of objects
-var res = [];
-Array.prototype.beFindObject = function(name) {
-  res.push(name);
-}
 
-var input = [
-  {name: "John", age: 26},
-  {name: "Doe", age: 30}
+Array.prototype.befindobject = function (key) {
+  const res = [];
+  this.map(function (object) {
+    res.push(object[key]);
+  });
+  return res;
+};
+
+var sampleinput = [
+  { name: "John", age: 26 },
+  { name: "siva", age: 27 },
 ];
 
-for(let ind=0;ind<input.length;ind++) {
-  input.beFindObject(input[ind].name);
-}
+console.log(sampleinput.befindobject("name"));
 
-console.log(res);
+// Alternative method
+// Array.prototype.befindobject = function(propertyName) {
+//   var propertyValue = [];
+//   for(var i=0;i<sampleinput.length;i++) {
+//     propertyValue[i] = sampleinput[i][propertyName];
+//   }
+//   console.log(propertyValue);
+// };
+
+// var sampleinput = [
+//   { name: "John", age: 26 },
+//   { name: "siva", age: 27 },
+// ];
+
+// sampleinput.befindobject("name");
